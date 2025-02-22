eaaabe113e9dffebcc8d5db76a527791381eea86
Aditya More <160622788+more-Aditya04@users.noreply.github.com>
2025-02-22 11:25:27 +0530

    Add files via upload

---
+++ src/components/Theme.js
@@ -0,0 +1,15 @@
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#000000",
+      },
+      secondary: {
+        main: "#5C2D91",
+      },
+
+    },
+  });
+  
+  export default theme;
